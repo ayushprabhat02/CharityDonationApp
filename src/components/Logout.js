3054714ae3fad8d29fde1adc0192418f5a993b2e
raj <ayush.prabhat02@gmail.com>
2022-02-13 16:30:51 +0530

    second commit

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -36,17 +36,3 @@
 //     fontSize: 16
 //   }
 // });
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-
-const Logout = () => {
-  return (
-    <View>
-      <Text>Logout</Text>
-    </View>
-  );
-};
-
-export default Logout;
-
-const styles = StyleSheet.create({});
